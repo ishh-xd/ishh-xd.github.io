a20257deaf2cca4b58f7d44fd2ba05c28a1dbdc4
Ishh <88816438+ishh-xd@users.noreply.github.com>
2024-09-13 15:15:31 +0530

    Update script.js

--- blogs/assets/script.js
+++ blogs/assets/script.js
@@ -1,16 +1,15 @@
+
 // Get the toggle button and the body
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 const toggleIcon = toggleButton.querySelector('img');
 
 // Check localStorage for the preferred theme and apply it
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-    body.classList.add(currentTheme);
-    if (currentTheme === 'light-mode') {
-        toggleIcon.src = 'https://img.icons8.com/ios-glyphs/30/000000/moon-symbol.png';
-    }
-}
+const currentTheme = localStorage.getItem('theme') || 'dark-mode'; // Default to dark mode
+body.classList.add(currentTheme);
+toggleIcon.src = currentTheme === 'light-mode'
+    ? 'https://img.icons8.com/ios-glyphs/30/000000/moon-symbol.png'
+    : 'https://img.icons8.com/ios-glyphs/30/ffffff/sun--v1.png';
 
 // Toggle between light and dark mode
 toggleButton.addEventListener('click', () => {
